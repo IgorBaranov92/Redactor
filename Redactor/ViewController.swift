2fe9c227ba6dedd44fd80f0b6bf520ecaf54b35f
Igor <d>
2021-03-10 10:46:06 +0300

    Initial Commit

---
+++ Redactor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Redactor
+//
+//  Created by Igor on 10.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
